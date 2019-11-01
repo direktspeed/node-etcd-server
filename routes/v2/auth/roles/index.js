c2667215769c6c5db0dc3d42f7d8290926f57ebc
Frank Lemanschik <frank@github.dspeed.eu>
2019-11-01 09:52:23 +0100

    Create index.js

---
+++ routes/v2/auth/roles/index.js
@@ -0,0 +1,12 @@
+// Return roles
+{
+  "role" : "fleet",
+  "permissions" : {
+    "kv" : {
+      "read" : [ "/fleet/" ],
+      "write": [ "/fleet/" ]
+    }
+  },
+  "grant" : {"kv": {...}},
+  "revoke": {"kv": {...}}
+}
